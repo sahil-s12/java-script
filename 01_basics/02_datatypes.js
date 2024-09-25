56653d8026db053dd608a6d72759c22f90b6f05e
sahil-s12 <sahil1092002786@gmail.com>
2024-09-25 16:14:17 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -6,3 +6,18 @@ console.log( 3
 )                   //code readiability should be high
 
 console.log("sahil");
+
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/fasle
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+// object 
+
+console.log(typeof undefined)
+console.log(typeof null)
