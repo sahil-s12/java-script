949f9c97f21492e82b6bf244ee712ff6987d3b20
sahil-s12 <sahil1092002786@gmail.com>
2024-09-25 14:12:56 +0000

    datatypes are done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+ "use strict" //treat whole program as the new version
+//  alert (3 + 3) we ar using nodejs not, web browser
+
+console.log( 3 
++ 3
+)                   //code readiability should be high
+
+console.log("sahil");
